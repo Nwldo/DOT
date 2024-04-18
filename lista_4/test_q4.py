cf6e356a5fd1dc20f92a1c6acf0059a3c8c538b2
Nwldo <regenildoliveira@yahoo.com.br>
2024-04-18 17:28:34 -0300

    Iniciando questão 4 da lista 4

--- lista_4/test_q4.py
+++ lista_4/test_q4.py
@@ -1,4 +1,17 @@
 """ 4) Escreva uma função que recebe uma lista com n números inteiros, e determina a
 maior soma de qualquer seguimento da lista. Ex: Ex: [5, -2, -2, -7, 3, 15, 10, -3, 9,
 -6, 4, 1] = 34 """
+def maior_soma_seguimento(lista):
+    max_atual = max_total = lista[0]
+    for i in lista:
+        if type(i)!=int or len(lista)== 0:
+            return Exception
+    for num in lista[1:]:
+        max_atual = max(num, max_atual + num)
+        max_total = max(max_total, max_atual)
+    return max_total
+
+# Testando a função
+print(maior_soma_seguimento([]))  # Deve retornar 34
+#assert maior_soma_seguimento([5, -2, -2, -7, 3, 15, 10, -3, 9, -6, 4, 1]) == 34 # testando valores válidos
 
